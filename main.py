745bfa884451b271af21f2bdcef11aa3d03814f3
Arkapravo-Ghosh <arkapravoghosh99@gmail.com>
2025-10-16 13:55:25 +0530

    feat: added support for nvidia gpu

--- main.py
+++ main.py
@@ -18,7 +18,12 @@ os.makedirs("./cache", exist_ok=True)
 os.environ["TRANSFORMERS_CACHE"] = "./models"
 os.environ["HF_HOME"] = "./models"
 
-device = "mps" if torch.backends.mps.is_available() else "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
 print(f"Using device: {device}")
 
 model_name = "openai/whisper-large"
